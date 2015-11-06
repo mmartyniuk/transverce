bcb3b693a5582c18848da16d10c2da7023d67566
chamer2010 <chamer2010@gmail.com>
2015-11-06 12:05:30 +0200

    added twitter-typeahead for add product's input, continued work with initial order page

--- js/createOrder.js
+++ js/createOrder.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
     $('.input-group.date').datepicker({});
+
+    var substringMatcher = function(strs) {
+	  	return function findMatches(q, cb) {
+	    var matches, substringRegex;
+
+	    // an array that will be populated with substring matches
+	    matches = [];
+
+	    // regex used to determine if a string contains the substring `q`
+	    substrRegex = new RegExp(q, 'i');
+
+	    // iterate through the pool of strings and for any string that
+	    // contains the substring `q`, add it to the `matches` array
+	    $.each(strs, function(i, str) {
+	      if (substrRegex.test(str)) {
+	        matches.push(str);
+	      }
+	    });
+
+	    cb(matches);
+	  };
+	};
+
+	var states = ['Product 1', 'Sub-product', 'One More Product'];
+
+	$('#product-select .typeahead').typeahead({
+	  hint: false,
+	  highlight: true,
+	  minLength: 1
+	},
+	{
+	  name: 'states',
+	  source: substringMatcher(states)
+	});
 });
